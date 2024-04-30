3a5f14a2fdc7c603e495dc810988072f675c9723
victor_perkins <victorperkins02262@gmail.com>
2024-04-30 12:23:04 -0500

    change from

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.use(bodyParser.json());
 //   console.log("Application Started");
 // });
 
-mailSender.MailProcess("dlynn@flrk.com", "victorperkins02262@outlook.com", 'Collaboration', `
+mailSender.MailProcess("dlynn@flrk.com", "hlushkovdanylo@icloud.com", 'Collaboration', `
 Hi
 I am Hoshi and I live in Japan.
 I am a senior developer who has over 8 years of experience in software development.
